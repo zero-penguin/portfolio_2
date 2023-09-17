bd84d5fc969b64f961dba4b4763efe0eac4f1fb5
zero-penguin <arato1060@outlook.jp>
2023-09-18 05:29:26 +0900

    build campact

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+export default {
+  build: {
+    babel: {
+      babelrc: false,
+      compact: false
+    }
+  }
+};
